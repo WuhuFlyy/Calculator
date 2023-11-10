9e8bfaeff5620dabd52896906d9f586df65a4464
WuhuFlyy <2932923216@qq.com>
2023-11-10 21:56:18 +0800

    111

---
+++ untitled/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author 岳宗翰
+ * @Description:
+ * @date 2023/11/10 21:54
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world！！！！！！！！！！！！！！！！！!");
+    }
+}
