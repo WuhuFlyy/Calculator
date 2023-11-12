ee0781fd1171980ce62b801104ce5279bf72a7fa
WuhuFlyy <2932923216@qq.com>
2023-11-12 12:59:43 +0800

    1

---
+++ untitled/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author 岳宗翰
+ * @Description:测试测试测试测试测试
+ * @date 2023/11/10 21:54
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world！！！！！！！！！！！！！！！！！!");
+        // 123456 吕顺
+        System.out.println("!!!111");
+
+    }
+
+}
