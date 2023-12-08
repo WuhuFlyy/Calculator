2e3ef379359616de44712a13630dedf5219179ad
WuhuFlyy <2932923216@qq.com>
2023-12-08 16:55:14 +0800

    还是矩阵bug

--- src/MatrixTest.java
+++ src/MatrixTest.java
@@ -14,13 +14,13 @@ public class MatrixTest {
 
 
         Fraction[][] m=new Fraction[5][5];
-        for(int i=1;i<=3;i++){
-            for(int j=1;j<=4;j++){
+        for(int i=1;i<=2;i++){
+            for(int j=1;j<=3;j++){
                 int a=in.nextInt();
                 m[i][j]=new Fraction(Integer.toString(a));
             }
         }
-        Matrix M=new Matrix(m,3,4);
+        Matrix M=new Matrix(m,2,3);
         System.out.println(M.getRank());
     }
 }
